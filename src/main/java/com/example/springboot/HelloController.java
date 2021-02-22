2d561c96bc41bc0b5547958aafcd7f4a1f07438d
Philip Whiting <phwhitin@cisco.com>
2021-02-22 18:52:27 -0500

    so far so good

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! I made an edit.";
+		return "Greetings from Spring Boot! I made an edit, again!";
 	}
 
 }
