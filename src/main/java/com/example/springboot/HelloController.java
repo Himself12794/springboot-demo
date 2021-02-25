46584398774c9a47986c986bd11dbca8c281c18c
Philip Whiting <phwhitin@cisco.com>
2021-02-25 10:01:13 -0500

    edit to show pipeline

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! I made an edit, again!";
+		return "Greetings from Spring Boot! This is to demo the pipeline.";
 	}
 
 }
